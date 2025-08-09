3b4e6586b16095c769ce3398a0455f7da420ecde
Thomas Putzer <putzerthomas55@gmail.com>
2025-08-09 18:03:24 +0200

    found bug why EE1 did not work to export, did not fix it

--- scenes/mass_momentum_conserving.py
+++ scenes/mass_momentum_conserving.py
@@ -13,14 +13,14 @@ if len(sys.argv) > 1:
     EXPORTS_BASE_DIR = "../exports/simple_plume_obstacle_3d_high/"
 
 with open(param_path) as f:
-	params = json.load(f)
+    params = json.load(f)
 
 # solver params
 dim = params["dimension"]
 gs = vec3(params["resolutionX"], params["resolutionY"], params["resolutionZ"])
 res = params["resolutionX"]
 if dim==2:
-	gs.z=1
+    gs.z=1
 s = Solver(name='main', gridSize = gs, dim=dim)
 
 # scene params
@@ -33,9 +33,9 @@ exportVideos = params["exportVideos"]
 exportVDBs = params["exportVDBs"]
 
 if exportVDBs and (exportImages or exportVideos):
-	raise Exception("Cannot export both VDBs and Images") 
+    raise Exception("Cannot export both VDBs and Images") 
 
-interpolationMethod = params["interpolationMethod"] # only important for tracingMethod == "RK4"
+interpolationMethod = params["interpolationMethod"] # only important for tracingMethod == "RK4" or doConserving ==  true
 tracingMethod = params["tracingMethod"] # only important for notCoserving (EE1: Explicit Euler Order 1, EE2: Explicit Euler with MAC cormack, RK$, Runge Kutta 4)
 
 title = params["title"]
@@ -69,20 +69,20 @@ flags.initDomain(boundaryWidth=bWidth)
 flags.fillGrid()
 
 if doOpen:
-	setOpenBound(flags, bWidth,'xXYzZ',FlagOutflow|FlagEmpty) 
+    setOpenBound(flags, bWidth,'xXYzZ',FlagOutflow|FlagEmpty) 
 
 if doObstacle:
-	obsPos = vec3(0.5, 0.55, 0.5)
-	obsVelVec = vec3(0.6,0.2,0.0) * (1./100.) * float(res) # velocity in grid units for 100 steps
-	obsSize = 0.11
+    obsPos = vec3(0.5, 0.55, 0.5)
+    obsVelVec = vec3(0.6,0.2,0.0) * (1./100.) * float(res) # velocity in grid units for 100 steps
+    obsSize = 0.11
 
-	obs = Sphere( parent=s, center=gs*obsPos, radius=res*obsSize)
-	phiObs = obs.computeLevelset()
+    obs = Sphere( parent=s, center=gs*obsPos, radius=res*obsSize)
+    phiObs = obs.computeLevelset()
 
-	setObstacleFlags(flags=flags, phiObs=phiObs, boundaryWidth=4) 
+    setObstacleFlags(flags=flags, phiObs=phiObs, boundaryWidth=4) 
 
-	flags.fillGrid()
-	obs.applyToGrid(grid=density, value=0.) # clear smoke inside, flags
+    flags.fillGrid()
+    obs.applyToGrid(grid=density, value=0.) # clear smoke inside, flags
 
 source = s.create(Cylinder, center=gs*vec3(0.5,0.12,0.5), radius=res*0.14, z=gs*vec3(0, 0.04, 0))
 
@@ -94,79 +94,79 @@ fillWithOnes( grid=vel_gamma )
 # guid and data collection
 gui = None
 if (GUI) and not exportVDBs:
-	gui = Gui()
-	gui.show( True ) 
-	#gui.pause()
+    gui = Gui()
+    gui.show( True ) 
+    #gui.pause()
 
 data_collector = Data_collectior(title=title ,base_dir=EXPORTS_BASE_DIR, params=params, export_data=exportData, 
-								 export_images=exportImages, export_videos=exportVideos, export_vdbs=exportVDBs, 
-								 trackable_grid_names=[["density", density], [], ["fixed_volume", innen0außen1], ["curl", curl], ["vel_magnitude", velocity_magnitude], [], [], []], 
-								 tracked_grids_indeces=[0, 2, 4], image_grids_indeces=[0], graph_grids=[["vel_magnitude", "max"], ["fixed_volume", "sum"]])
+                                 export_images=exportImages, export_videos=exportVideos, export_vdbs=exportVDBs, 
+                                 trackable_grid_names=[["density", density], [], ["fixed_volume", innen0außen1], ["curl", curl], ["vel_magnitude", velocity_magnitude], [], [], []], 
+                                 tracked_grids_indeces=[0, 2, 4], image_grids_indeces=[0], graph_grids=[["vel_magnitude", "max"], ["fixed_volume", "sum"]])
 
 data_collector.init()
 
 firstFrame = True
 #main loop
 while s.timeTotal < params["max_time"] : #and data_collector.current_frame < 70:
-	
-	computeVelocityMagnitude(dest=velocity_magnitude, vel=vel)
-	maxvel = getMaxVal(grid=velocity_magnitude, flags=flags) # flags param does nothign for now
+    
+    computeVelocityMagnitude(dest=velocity_magnitude, vel=vel)
+    maxvel = getMaxVal(grid=velocity_magnitude, flags=flags) # flags param does nothign for now
 
-	if firstFrame:
-		maxvel = 15     
-		firstFrame = False
+    if firstFrame:
+        maxvel = 15     
+        firstFrame = False
 
-	s.adaptTimestep(maxvel)
+    s.adaptTimestep(maxvel)
 
-	print(f"cfl number?: {maxvel * s.timestep}, timestep: {s.timestep}, maxvel: {maxvel}")
-	mantaMsg('\nFrame %i, simulation time %f' % (s.frame, s.timeTotal))
+    print(f"cfl number?: {maxvel * s.timestep}, timestep: {s.timestep}, maxvel: {maxvel}")
+    mantaMsg('\nFrame %i, simulation time %f' % (s.frame, s.timeTotal))
 
-	if s.timeTotal<3000:
-		source.applyToGrid(grid=density, value=1)
+    if s.timeTotal<3000:
+        source.applyToGrid(grid=density, value=1)
 
-	# optionally, dissolve smoke
-	# dissolveSmoke(flags=flags, density=density, speed=4)
+    # optionally, dissolve smoke
+    # dissolveSmoke(flags=flags, density=density, speed=4)
 
-	#Advection
-	if not doConserving:
-		if tracingMethod.startswith("EE"):
-			order = int(tracingMethod[-1])  # Extracts 1 or 2 from "EE1" or "EE2"
-			advectSemiLagrange(flags=flags, vel=vel, grid=density,      order=order) # ziemlich scheiße, hauptsachlich da es explicit Euler verwendet, nicht RK4 wie simpleSLAdvect 
-			advectSemiLagrange(flags=flags, vel=vel, grid=innen0außen1, order=order) # ziemlich scheiße, hauptsachlich da es explicit Euler verwendet, nicht RK4 wie simpleSLAdvect
-			advectSemiLagrange(flags=flags, vel=vel, grid=vel,          order=order) # ziemlich scheiße, hauptsachlich da es explicit Euler verwendet, nicht RK4 wie simpleSLAdvect
+    #Advection
+    if not doConserving:
+        if tracingMethod.startswith("EE"):
+            order = int(tracingMethod[-1])  # Extracts 1 or 2 from "EE1" or "EE2"
+            advectSemiLagrange(flags=flags, vel=vel, grid=density,      order=order) # ziemlich scheiße, hauptsachlich da es explicit Euler verwendet, nicht RK4 wie simpleSLAdvect 
+            advectSemiLagrange(flags=flags, vel=vel, grid=innen0außen1, order=order) # ziemlich scheiße, hauptsachlich da es explicit Euler verwendet, nicht RK4 wie simpleSLAdvect
+            advectSemiLagrange(flags=flags, vel=vel, grid=vel,          order=order) # ziemlich scheiße, hauptsachlich da es explicit Euler verwendet, nicht RK4 wie simpleSLAdvect
 
-			setOutflowToZero(flags=flags, grid=density)
-			setOutflowToZero(flags=flags, grid=innen0außen1)
-			setOutflowToZero(flags=flags, grid=vel)
+            setOutflowToZero(flags=flags, grid=density)
+            setOutflowToZero(flags=flags, grid=innen0außen1)
+            setOutflowToZero(flags=flags, grid=vel)
 
-		elif tracingMethod == "RK4":
-			simpleSLAdvect(flags=flags, vel=vel, grid=density,     interpolationType=interpolationMethod) # 0 = Trilinear, 1 = Cubic, 2= Polynomial Interpolation, 3 = monotonue cubib (hermite)
-			simpleSLAdvect(flags=flags, vel=vel, grid=innen0außen1,interpolationType=interpolationMethod) # 0 = Trilinear, 1 = Cubic, 2= Polynomial Interpolation, 3 = monotonue cubib (hermite)
-			simpleSLAdvect(flags=flags, vel=vel, grid=vel,         interpolationType=interpolationMethod) # 0 = Trilinear, 1 = Cubic, 2= Polynomial Interpolation, 3 = monotonue cubib (hermite)
+        elif tracingMethod == "RK4":
+            simpleSLAdvect(flags=flags, vel=vel, grid=density,     interpolationType=interpolationMethod) # 0 = Trilinear, 1 = Cubic, 2= Polynomial Interpolation, 3 = monotonue cubib (hermite)
+            simpleSLAdvect(flags=flags, vel=vel, grid=innen0außen1,interpolationType=interpolationMethod) # 0 = Trilinear, 1 = Cubic, 2= Polynomial Interpolation, 3 = monotonue cubib (hermite)
+            simpleSLAdvect(flags=flags, vel=vel, grid=vel,         interpolationType=interpolationMethod) # 0 = Trilinear, 1 = Cubic, 2= Polynomial Interpolation, 3 = monotonue cubib (hermite)
 
-	else:
-		#simpleSLAdvect(flags=flags, vel=vel, grid=density,      interpolationType=1) # 0 = Trilinear, 1 = Catmull Rom
-		massMomentumConservingAdvect( flags=flags, vel=vel, grid=density, gammaCumulative=density_gamma,          interpolationType=interpolationMethod)
-		massMomentumConservingAdvect( flags=flags, vel=vel, grid=innen0außen1, gammaCumulative=innen0außen1_gamma,interpolationType=interpolationMethod)
-		massMomentumConservingAdvect( flags=flags, vel=vel, grid=vel, gammaCumulative=vel_gamma,                  interpolationType=interpolationMethod)
+    else:
+        #simpleSLAdvect(flags=flags, vel=vel, grid=density,      interpolationType=1) # 0 = Trilinear, 1 = Catmull Rom
+        massMomentumConservingAdvect( flags=flags, vel=vel, grid=density, gammaCumulative=density_gamma,          interpolationType=interpolationMethod)
+        massMomentumConservingAdvect( flags=flags, vel=vel, grid=innen0außen1, gammaCumulative=innen0außen1_gamma,interpolationType=interpolationMethod)
+        massMomentumConservingAdvect( flags=flags, vel=vel, grid=vel, gammaCumulative=vel_gamma,                  interpolationType=interpolationMethod)
 
-	if doOpen:
-		resetOutflow(flags=flags,real=density) 
+    if doOpen:
+        resetOutflow(flags=flags,real=density) 
 
-	# Pressure Solve
-	setWallBcs(flags=flags, vel=vel)    
-	addBuoyancy(density=density, vel=vel, gravity=vec3(0,-4e-3,0), flags=flags)
+    # Pressure Solve
+    setWallBcs(flags=flags, vel=vel)    
+    addBuoyancy(density=density, vel=vel, gravity=vec3(0,-4e-3,0), flags=flags)
 
-	solvePressure(flags=flags, vel=vel, pressure=pressure)
-	
-	# Output
-	calculateCurl(vel=vel, curl=curl, flags=flags)
-	computeVelocityMagnitude(dest=velocity_magnitude, vel=vel)
-	maxVel = getMaxVal(grid=velocity_magnitude, flags=flags) # flags param does nothign for now
-	data_collector.step(solver=s, flags=flags, maxVel=maxVel, gui=gui, objects=[density, velocity_magnitude])
+    solvePressure(flags=flags, vel=vel, pressure=pressure)
+    
+    # Output
+    calculateCurl(vel=vel, curl=curl, flags=flags)
+    computeVelocityMagnitude(dest=velocity_magnitude, vel=vel)
+    maxVel = getMaxVal(grid=velocity_magnitude, flags=flags) # flags param does nothign for now
+    data_collector.step(solver=s, flags=flags, maxVel=maxVel, gui=gui, objects=[density, velocity_magnitude])
 
-	timings.display()    
-	s.step()
+    timings.display()    
+    s.step()
 
 data_collector.finish()
 data_collector.printStats()
